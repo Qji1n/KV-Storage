af7fa9915c322b8e3fc41d525b0c0a9c144416ba
VladimirLezhnin <115662980+VladimirLezhnin@users.noreply.github.com>
2023-10-15 15:26:40 +0500

    Update kv_storage.py
    Переход от json к bson, от zlib к lz4, добавление класса хэш-функции, добавление метода получения всех ключей, создание метода добавления нескольких пар ключ-значение за раз, проблема - непонятное поведение словаря и хэш-функции, увеличение ключей при добавлении одних и тех же ключей, в следствии этого вырастает объём словаря и увеличиваются затраты на архивацию/обратную архивацию

--- kv_storage.py
+++ kv_storage.py
@@ -1,45 +1,73 @@
-import json
 import os
 import zlib
+import bson
+from datetime import datetime
+import lz4.frame
+import hashlib
 
+class Hasher:
+    @staticmethod
+    def hash(key):
+        if isinstance(key, str):
+            return int(hashlib.md5(key.encode('utf-8')).hexdigest(), 16)
+        else:
+            raise TypeError("Key must be a string")
 
 class KVStorage:
     def __init__(self, filename):
         self.filename = filename
         self.data = {}
+        self.hasher = Hasher()
         if os.path.exists(filename):
             self.load()
 
     def set(self, key, value):
-        self.data[key] = value
+        hashed_key = self.hasher.hash(key)
+        self.data[hashed_key] = value
         self.save()
 
-    def get(self, key):
-        return self.data.get(key, None)
+    def set_multiple(self, key_value_dict):
+        for key, value in key_value_dict.items():
+            self.set(key, value)
+
+    def get(self, key, default=None):
+        hashed_key = self.hasher.hash(key)
+        return self.data.get(hashed_key, default)
 
     def delete(self, key):
-        if key in self.data:
-            del self.data[key]
+        hashed_key = self.hasher.hash(key)
+        if hashed_key in self.data:
+            del self.data[hashed_key]
             self.save()
+
+    def get_all_keys(self):
+        return [key for key in self.data.keys()]
 
     def save(self):
         with open(self.filename, 'wb') as f:
-            compressed = zlib.compress(json.dumps(self.data).encode('utf-8'))
+            packed_data = bson.dumps(self.data)
+            compressed = lz4.frame.compress(packed_data)
             f.write(compressed)
 
     def load(self):
         with open(self.filename, 'rb') as f:
             compressed = f.read()
-            decompressed = zlib.decompress(compressed).decode('utf-8')
-            self.data = json.loads(decompressed)
+            decompressed = lz4.frame.decompress(compressed)
+            self.data = bson.loads(decompressed)
 
 
-kv = KVStorage('storage.json')
-kv.set('key1', 'value1')
-kv.set('key2', 'value2')
-kv.set('key3', 'value3')
+# Пример использования:
+storage = KVStorage('data.kvs')
+f = open('log2.txt')
+value3 = f.read()
+time1 = datetime.now()
+storage.set('key1', 'value1')
+storage.set('key2', 'value2')
+storage.set('key3', value3)
+print(storage.get_all_keys())
+print(datetime.now() - time1)
+a = storage.get('key1')
+print(datetime.now() - time1)
+f.close()
 
-print(kv.get('key1'))
-print(kv.get('key2'))
-print(kv.get('key3'))
-print(kv.get('key5'))
+
